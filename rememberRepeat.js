26d0e935bd8fa41a025bde7f872fa7d97a21336a
Jaroslaw Chaykin <jaroslaw.chaikin@gmail.com>
2020-10-19 23:33:41 +0300

    add - function urlGenerator add - main.js

--- rememberRepeat.js
+++ rememberRepeat.js
@@ -27,3 +27,14 @@ blocks.onclick = function(e) {
         startGame(id)
     }
 }
+
+function urlGenerator(domain) {
+    return function (url) {
+        return `https://${url}.${domain}`
+    }
+}
+
+const comUrl = urlGenerator('com')
+const ruUrl = urlGenerator('ru')
+
+console.log(comUrl('google'))
