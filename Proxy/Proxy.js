c4bb0ee794c36781dc3c9f67021561310fc4bc15
Jaroslaw Chaykin <jaroslaw.chaikin@gmail.com>
2020-11-25 14:26:09 +0300

    Add folder Proxy

---
+++ Proxy/Proxy.js
@@ -0,0 +1,63 @@
+const person = {
+    name: 'Jaroslaw',
+    age: 21,
+    job: 'FrontEnd'
+}
+const op = new Proxy(person, {
+    get(target, prop) {
+        return target[prop]
+    },
+    set(target, prop, value) {
+        if (prop in target) {
+            target.prop = value
+        } else {
+            throw new Error(`No ${prop}`)
+        }
+    },
+    has(target, prop) {
+        return ['age', 'name', 'job'].includes(prop)
+    },
+    deleteProperty(target, prop) {
+        console.log('Deleting...', prop)
+        delete target[prop]
+    }
+})
+
+// Functions
+
+const log = text => `log: ${text}`
+
+const fp = new Proxy(log, {
+    apply(target, thisArg, args) {
+        console.log('Calling...')
+
+        return target.apply(thisArg, args).toUpperCase()
+    }
+})
+
+// Class
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+const pp = new Proxy(Person, {
+    construct(target, args) {
+        console.log('Construct')
+
+        return new Proxy(new target(...args), {
+            get(t, prop) {
+                console.log(`Getting prop "${prop}"`)
+
+                return t[prop]
+            }
+        })
+    }
+})
+
+const p = new pp('Jaroslaw', 21)
+
+console.log(p.name)
