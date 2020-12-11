3f9d91acfdfefba92b0cc4f4f3af9dbde251c843
Jaroslaw Chaykin <jaroslaw.chaikin@gmail.com>
2020-12-11 22:36:44 +0300

    Add style to future shopping cart

--- ShoppingСart/ShoppingСart.js
+++ ShoppingСart/ShoppingСart.js
@@ -1,18 +1,18 @@
-function loading() {
-    let num = 0;
-    const load = document.querySelector('.label');
-    const loaded = document.querySelector('.loaded');
-
-    let interval1 = setInterval(() => {
-        num++
-        load.innerHTML = `Loading ${num}%`;
-        loaded.style.width = num + '%';
-        if (num === 101) {
-            loaded.style.width = 0 + '%';
-            load.innerHTML = `Загрузка завершена!`;
-            clearInterval(interval1)
-        }
-    }, 30)
-}
-
-loading()
+// function loading() {
+//     let num = 0;
+//     const load = document.querySelector('.label');
+//     const loaded = document.querySelector('.loaded');
+//
+//     let interval1 = setInterval(() => {
+//         num++
+//         load.innerHTML = `Loading ${num}%`;
+//         loaded.style.width = num + '%';
+//         if (num === 101) {
+//             loaded.style.width = 0 + '%';
+//             load.innerHTML = `Загрузка завершена!`;
+//             clearInterval(interval1)
+//         }
+//     }, 30)
+// }
+//
+// loading()
